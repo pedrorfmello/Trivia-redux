d127005aba058c0ccf3a340a306e72ee1da67452
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-25 08:53:33 -0300

    feat: adiciona um helper que retorna as questões geradas pela API

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,7 @@
 import * as local from './localStorage';
 
 const URL_TRIVIA_TOKEN = 'https://opentdb.com/api_token.php?command=request';
+const URL_TRIVIA_QUESTION = 'https://opentdb.com/api.php?amount=5&token=';
 
 // Pega um token pelo fetch.
 export const fetchTriviaToken = async () => {
@@ -14,4 +15,12 @@ export const getTriviaToken = async () => {
   const token = await fetchTriviaToken();
   local.setLocalToken(token);
   if (token) return token;
+};
+
+// Fazendo o fetch nas perguntas.
+export const fetchTriviaQuestion = async () => {
+  const token = await getTriviaToken();
+  const response = await fetch(URL_TRIVIA_QUESTION + token);
+  const data = await response.json();
+  return data.results;
 };
