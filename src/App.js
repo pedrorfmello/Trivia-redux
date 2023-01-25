02224285e6f6d4c61770b5b8e868d51eb3457d35
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-25 03:35:31 -0300

    Iniciando tela de jogo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Jogo from './pages/Jogo';
 import './App.css';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/jogo" component={ Jogo } />
     </Switch>
   );
 }
