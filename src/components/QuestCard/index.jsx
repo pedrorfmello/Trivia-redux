6cabcf8b7c64de7a410725ea39a044311b16d988
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-26 05:21:58 -0300

    feat: adiciona um contador + calculo de score
    O contador desabilita as opções de resposta ao zerar.
    
    O score é calculado seguindo a regra de negócio:
    
    10 + tempo restante * dificuldade
    
    fácil = 1 / médio = 2 / difícil = 3
    
    Ao finalizar as 5 perguntas o jogador é redirecionado para a tela de feedback

--- src/components/QuestCard/index.jsx
+++ src/components/QuestCard/index.jsx
@@ -1,47 +1,113 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './QuestCard.css';
+import { useDispatch } from 'react-redux';
+import { changeScore } from '../../redux/actions/game';
 
-const QuestCard = (quest, setNextBtnStatus) => {
-  const { question, category, answers, correct } = quest;
+const QuestCard = ({
+  quest, timer, setTimer, gameTimeout, setGameTimeout, setNextBtnStatus }) => {
+  const { question, category, answers, correct, difficulty } = quest;
+
+  const dispatch = useDispatch();
+
+  // Cronômetro do jogo
+  useEffect(() => {
+    const intervalTime = 1000;
+
+    const interval = setInterval(() => {
+      if (gameTimeout) {
+        return () => clearInterval(interval);
+      }
+      if (timer > 0) {
+        setTimer(timer - 1);
+      } else {
+        setGameTimeout(true);
+        setNextBtnStatus(true);
+      }
+    }, intervalTime);
+
+    return () => clearInterval(interval);
+  }, [gameTimeout, setGameTimeout, setTimer, timer, setNextBtnStatus]);
 
   // Seleciona todos as respostas e adiciona a classe 'answer-wrong' apenas nas respostas erradas
   const findAndSetClassName = () => {
     const allButtons = document.querySelectorAll('.answer-btn');
-    allButtons.forEach((btn) => {
-      if (!btn.classList.contains('answer-correct')) {
-        btn.classList.add('answer-wrong');
+    let wrongIndex = 0;
+
+    allButtons.forEach((button) => {
+      if (button.innerText === correct) {
+        button.classList.add('answer-correct');
+        button.setAttribute('data-testid', 'correct-answer');
+      } else {
+        button.classList.add('answer-wrong');
+        button.setAttribute('data-testid', `wrong-answer-${wrongIndex}`);
+        wrongIndex += 1;
       }
     });
   };
 
+  // Calcula a pontuação do jogador seguindo a regra de negócio
+  const calculateScore = () => {
+    const baseValue = 10;
+    const multipliers = { hard: 3, medium: 2, easy: 1 };
+
+    const score = baseValue + (timer * multipliers[difficulty]);
+
+    dispatch(changeScore({
+      score,
+      assertions: 1,
+    }));
+  };
+
   // Confere se a resposta seleciona é a correta
   const handleClick = ({ target }) => {
+    findAndSetClassName();
+    setGameTimeout(true);
     if (target.innerText === correct) {
-      target.classList.add('answer-correct');
+      calculateScore();
       setNextBtnStatus(true);
-      findAndSetClassName();
+    } else {
+      setNextBtnStatus(true);
     }
   };
 
   return (
     <>
       <div>
-        <h3>{ category }</h3>
+        <h3 data-testid="question-category">{ category }</h3>
         <h2>{ question }</h2>
       </div>
-      <div>
+      <h1>{ timer }</h1>
+      <div data-testid="answer-options">
         { answers.map((answer, index) => (
           <button
             key={ index }
             type="button"
             className="answer-btn"
             onClick={ handleClick }
+            disabled={ gameTimeout }
+            data-testid="question-text"
           >
             { answer }
           </button>)) }
       </div>
     </>
   );
+};
+
+QuestCard.propTypes = {
+  quest: PropTypes.shape({
+    question: PropTypes.string.isRequired,
+    answers: PropTypes.arrayOf(PropTypes.string).isRequired,
+    correct: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    difficulty: PropTypes.string.isRequired,
+  }).isRequired,
+  timer: PropTypes.number.isRequired,
+  setTimer: PropTypes.func.isRequired,
+  gameTimeout: PropTypes.bool.isRequired,
+  setGameTimeout: PropTypes.func.isRequired,
+  setNextBtnStatus: PropTypes.func.isRequired,
 };
 
 export default QuestCard;
--- src/pages/Jogo/index.jsx
+++ src/pages/Jogo/index.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import QuestCard from '../../components/QuestCard';
 import { fetchTriviaQuestion } from '../../helpers/api';
 
 const Jogo = () => {
+  const history = useHistory();
+  const maxQuests = 5;
   // Iniciando os estados do compomente
   const [isLoading, setIsLoading] = useState(true);
   const [nextBtnStatus, setNextBtnStatus] = useState(false);
   const [allQuestions, setAllQuestions] = useState([]);
   const [index, setIndex] = useState(0);
+  // Estados do componente QuestCard
+  const questTime = 30;
+  const [timer, setTimer] = useState(questTime);
+  const [gameTimeout, setGameTimeout] = useState(false);
 
   // Embaralha um array
   const shuffle = (array) => {
@@ -24,14 +31,17 @@ const Jogo = () => {
         correct_answer: correct,
         question,
         category,
+        difficulty,
       }) => {
         setAllQuestions((prevState) => [...prevState, {
           question,
           category,
           answers: shuffle([...incorrect, correct]),
           correct,
+          difficulty,
         }]);
       });
+      // Remove a mensagem "Carregando..." da tela
       setIsLoading(false);
     };
 
@@ -44,19 +54,16 @@ const Jogo = () => {
     fetchQuestions();
   }, []);
 
-  // Remove a estilização que define a pergunta certa e errada
-  const resetClassList = () => {
-    const allButtons = document.querySelectorAll('.answer-btn');
-    allButtons.forEach((btn) => {
-      btn.className = 'answer-btn';
-    });
-  };
-
+  // Passa para a próxima pergunta
   const handleNext = () => {
-    // Passa para a próxima pergunta
+    if (index === maxQuests - 1) {
+      history.push('/feedback');
+    }
     setIndex(index + 1);
-
-    resetClassList();
+    // Reseta o tempo do contador
+    setTimer(questTime);
+    // Reativa o contador
+    setGameTimeout(false);
   };
 
   return (
@@ -64,8 +71,18 @@ const Jogo = () => {
       <header>AQUI</header>
       <div className="quest-body">
         <div className="quest-card">
-          { isLoading ? <h1>LOADING...</h1>
-            : QuestCard(allQuestions[index], setNextBtnStatus) }
+          { isLoading
+            ? <h1>Carregando...</h1>
+            : (
+              <QuestCard
+                key={ index }
+                quest={ allQuestions[index] }
+                timer={ timer }
+                setTimer={ setTimer }
+                gameTimeout={ gameTimeout }
+                setGameTimeout={ setGameTimeout }
+                setNextBtnStatus={ setNextBtnStatus }
+              />) }
           { nextBtnStatus
             ? <button type="button" onClick={ handleNext }>Next</button>
             : ''}
