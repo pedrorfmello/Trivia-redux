393622327e3fb739c7ddb4e275195edadc25bdec
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-27 19:50:38 -0300

    feat: salva a pontuação do jogador no ranking

--- src/pages/Feedback/index.jsx
+++ src/pages/Feedback/index.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
+import { hashEmail } from '../../helpers/hashEmail';
+import { setLocalRanking } from '../../helpers/localStorage';
 
 const Feedback = () => {
-  const { score, assertions } = useSelector((state) => state.player);
+  const { score, assertions, playerName, gravatarEmail } = useSelector(
+    (state) => state.player,
+  );
   const condition = 3;
+
+  useEffect(() => {
+    const playerScoreInfo = {
+      playerName,
+      score,
+      photo: hashEmail(gravatarEmail),
+    };
+
+    setLocalRanking(playerScoreInfo);
+  });
 
   return (
     <>
@@ -20,22 +34,14 @@ const Feedback = () => {
         </div>
         <div>
           Você acertou:
-          <span data-testid="feedback-total-question">{ assertions }</span>
+          <span data-testid="feedback-total-question">{assertions}</span>
         </div>
       </div>
       <Link to="/">
-        <input
-          data-testid="btn-play-again"
-          type="button"
-          value="Jogar Novamente"
-        />
+        <input data-testid="btn-play-again" type="button" value="Jogar Novamente" />
       </Link>
       <Link to="/ranking">
-        <input
-          data-testid="btn-ranking"
-          type="button"
-          value="Ranking"
-        />
+        <input data-testid="btn-ranking" type="button" value="Ranking" />
       </Link>
     </>
   );
