1c2efdeccf3808b3af587898c257f6c13ba64f9e
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-29 09:03:44 -0300

    Updates

---
+++ precache-manifest.21bb7d9e82eb907c3c3c308b76958769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d58774071387dafd5073a2ec8c9abd7",
+    "url": "/Trivia-redux/index.html"
+  },
+  {
+    "revision": "dea10b90040fe2dd036b",
+    "url": "/Trivia-redux/static/css/main.99b83c03.chunk.css"
+  },
+  {
+    "revision": "75b82bb6b01185b011f9",
+    "url": "/Trivia-redux/static/js/2.1e671930.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/Trivia-redux/static/js/2.1e671930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea10b90040fe2dd036b",
+    "url": "/Trivia-redux/static/js/main.2e8eedd5.chunk.js"
+  },
+  {
+    "revision": "fe248f2912b3b4d46f86",
+    "url": "/Trivia-redux/static/js/runtime-main.267f938d.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia-redux/static/media/trivia.466d153e.png"
+  }
+]);
