0ba97f986edc1fb9c9389041a57d2d4e87e51928
Pedro Mello <pedro.mello.rj@gmail.com>
2023-01-29 09:30:01 -0300

    Deploy feito com sucesso!

---
+++ build/precache-manifest.0f7b6c0968c75d6d1379325138272db1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e00fdd1ebea9fbde76b3ba8ac947196",
+    "url": "/Trivia-redux/index.html"
+  },
+  {
+    "revision": "68e97d2f03f29581c91e",
+    "url": "/Trivia-redux/static/css/main.99b83c03.chunk.css"
+  },
+  {
+    "revision": "7cdcfda535facab775e5",
+    "url": "/Trivia-redux/static/js/2.60e3980f.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/Trivia-redux/static/js/2.60e3980f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e97d2f03f29581c91e",
+    "url": "/Trivia-redux/static/js/main.a5c30071.chunk.js"
+  },
+  {
+    "revision": "fe248f2912b3b4d46f86",
+    "url": "/Trivia-redux/static/js/runtime-main.267f938d.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia-redux/static/media/trivia.466d153e.png"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import Store from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ Store }>
-      <App />
-    </Provider>
+    <HashRouter basename={ process.env.PUBLIC_URL }>
+      <Provider store={ Store }>
+        <App />
+      </Provider>
+    </HashRouter>
   </BrowserRouter>,
 
   document.getElementById('root'),
